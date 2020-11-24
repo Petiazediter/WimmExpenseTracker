4911df9abdb3b0be0c0cc637b5477a80e30cb471
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-11-24 20:43:32 +0100

    Main activity moved to product_activity package.

--- app/src/main/java/com/codecool/wimmexpensetracker/product_activity/MainActivity.kt
+++ app/src/main/java/com/codecool/wimmexpensetracker/product_activity/MainActivity.kt
@@ -1,9 +1,11 @@
-package com.codecool.wimmexpensetracker
+package com.codecool.wimmexpensetracker.product_activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.codecool.wimmexpensetracker.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
