5fc4942e93ad54fa796094cbeb90bb8c9cefc90e
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-11-25 15:09:31 +0100

    Refactored some code snippets.

--- app/src/main/java/com/codecool/wimmexpensetracker/product_activity/MainActivity.kt
+++ app/src/main/java/com/codecool/wimmexpensetracker/product_activity/MainActivity.kt
@@ -12,8 +12,8 @@ import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity(), MainActivityContractor {
 
-    val tabs = listOf(
-            FragmentWrapper(HomeFragment(), R.string.home_fragment_title,R.drawable.ic_baseline_home_24)
+    private val tabs = listOf(
+            FragmentWrapper(HomeFragment(), R.string.home_fragment_title, R.drawable.ic_baseline_home_24)
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,9 +23,9 @@ class MainActivity : AppCompatActivity(), MainActivityContractor {
         val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
         val viewPager2 = findViewById<ViewPager2>(R.id.view_pager_2)
 
-        viewPager2.adapter = ViewPagerAdapter(tabs,supportFragmentManager,lifecycle)
+        viewPager2.adapter = ViewPagerAdapter(tabs, supportFragmentManager, lifecycle)
 
-        TabLayoutMediator(tabLayout,viewPager2){tab,position ->
+        TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
             tab.text = resources.getString(tabs[position].title)
             tab.icon = ContextCompat.getDrawable(applicationContext, tabs[position].drawableId)
         }.attach()
--- app/src/main/java/com/codecool/wimmexpensetracker/product_activity/ViewPagerAdapter.kt
+++ app/src/main/java/com/codecool/wimmexpensetracker/product_activity/ViewPagerAdapter.kt
@@ -6,7 +6,7 @@ import androidx.fragment.app.FragmentPagerAdapter
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class ViewPagerAdapter( val listOfFragments : List<FragmentWrapper>,fragmentManager: FragmentManager,lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager,lifecycle) {
+class ViewPagerAdapter(private val listOfFragments : List<FragmentWrapper>, fragmentManager: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager,lifecycle) {
     override fun getItemCount(): Int = listOfFragments.size
     override fun createFragment(position: Int): Fragment = listOfFragments[position].fragment
 }
