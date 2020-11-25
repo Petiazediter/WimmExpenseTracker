f1f64cb219feabe285615be5cd30aff9ac54c9fd
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-11-25 10:54:50 +0100

    Added main activity contractor which is responsible for set the menu and the submenu text

--- app/src/main/java/com/codecool/wimmexpensetracker/product_activity/MainActivity.kt
+++ app/src/main/java/com/codecool/wimmexpensetracker/product_activity/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.codecool.wimmexpensetracker.product_activity
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.codecool.wimmexpensetracker.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainActivityContractor {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun setMenuTitle(menuTitle: String) {
+        findViewById<TextView>(R.id.menu_title)
+                .text = menuTitle
+    }
+
+    override fun setPageTitle(pageTitle: String) {
+        findViewById<TextView>(R.id.sub_menu)
+                .text = pageTitle
     }
 }
