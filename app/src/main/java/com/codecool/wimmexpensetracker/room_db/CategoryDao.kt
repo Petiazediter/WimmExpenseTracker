9a16c182d54aec12e9f54e8ce851e11253457fa8
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-12-02 11:40:31 +0100

    category dao extended.

--- app/src/main/java/com/codecool/wimmexpensetracker/room_db/CategoryDao.kt
+++ app/src/main/java/com/codecool/wimmexpensetracker/room_db/CategoryDao.kt
@@ -1,6 +1,8 @@
 package com.codecool.wimmexpensetracker.room_db
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.Query
 
 @Dao
@@ -8,5 +10,9 @@ interface CategoryDao {
     @Query("SELECT * FROM categories")
     fun getAllCategories() : List<Category>
 
+    @Insert
+    fun insertCategory(category: Category)
 
+    @Delete
+    fun deleteCategory(category: Category)
 }
