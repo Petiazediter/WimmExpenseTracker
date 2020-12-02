ae67d11aaa8c1ec2679f556d2e2548c94020b803
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-12-02 11:36:53 +0100

    Added Room Database to the project.

---
+++ app/src/main/java/com/codecool/wimmexpensetracker/room_db/Category.kt
@@ -0,0 +1,12 @@
+package com.codecool.wimmexpensetracker.room_db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Category (
+    @PrimaryKey val uId : Int,
+    @ColumnInfo(name="category_name") val categoryName : String,
+    @ColumnInfo(name="category_color") val colorId : Int
+)
