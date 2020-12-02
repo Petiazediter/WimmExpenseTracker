f5e510580f3e621785e034aeaef2494ab82fd919
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-12-03 00:37:03 +0100

    Chart is now customized.

--- app/src/main/java/com/codecool/wimmexpensetracker/home_fragment/HomeFragment.kt
+++ app/src/main/java/com/codecool/wimmexpensetracker/home_fragment/HomeFragment.kt
@@ -11,6 +11,7 @@ import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.anychart.core.ui.ScrollBar
 import com.anychart.graphics.vector.Fill
 import com.codecool.wimmexpensetracker.R
 import com.codecool.wimmexpensetracker.data.SharedPreferenceController
@@ -18,6 +19,7 @@ import com.codecool.wimmexpensetracker.mvvm.view_models.HomeFragmentViewModel
 import com.codecool.wimmexpensetracker.product_activity.MainActivityContractor
 import com.codecool.wimmexpensetracker.room_db.Expense
 import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
@@ -76,58 +78,50 @@ class HomeFragment : Fragment() {
         })
     }
 
-   /* private fun setUpChart(list : List<Pair<Int,List<Expense>>>){
-        Log.d("HomeFragment", "setUpChart() -> ${list.size}")
-        val cartesian = AnyChart.column()
-        val data = ArrayList<DataEntry>()
-        for ( value in list ){
-            data.add ( ValueDataEntry(
-                    Month.of(value.first).name,
-                    value.second.map{it.amount}.sum() )
-            )
-        }
-
-        val column = cartesian.column(data)
-        column.tooltip()
-            .titleFormat("{%X}")
-            .position(Position.CENTER)
-            .anchor(Anchor.CENTER_BOTTOM)
-            .offsetX(0)
-            .offsetY(5)
-
-        cartesian.animation(true)
-        cartesian.yScale().minimum(0)
-
-        cartesian.interactivity().hoverMode(HoverMode.BY_X);
-
-        cartesian.xAxis(0).title("asd");
-        cartesian.yAxis(0).title("asd");
-
-        anyChartView.setChart(cartesian)
-    }*/
 
     private fun setUpChart(list: List<Pair<Int, List<Expense>>>){
-        anyChartView.setDrawBarShadow(false)
         anyChartView.setDrawValueAboveBar(true)
-        anyChartView.description.isEnabled = false
         anyChartView.setMaxVisibleValueCount(5)
-        anyChartView.setPinchZoom(false)
-        anyChartView.setDrawGridBackground(false)
+        anyChartView.description.isEnabled = false
+
+        val xAxis = anyChartView.xAxis
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        xAxis.setDrawGridLines(false)
+        xAxis.labelCount = 7
+        xAxis.granularity = 1f
+        xAxis.isEnabled = false
+
+        val yAxis = anyChartView.axisLeft
+        yAxis.axisMinimum = 0f
+        yAxis.setDrawGridLines(false)
+        yAxis.spaceTop = 15f
+
+        val yAxis2 = anyChartView.axisRight
+        yAxis2.axisMinimum = 0f
+        yAxis2.spaceTop = 15f
+        yAxis2.setDrawGridLines(false)
+
 
         val barDataSetList = ArrayList<IBarDataSet>()
         list.forEachIndexed{ index,pair ->
             val barEntryList = ArrayList<BarEntry>()
             barEntryList.add ( BarEntry(index.toFloat(),pair.second.map { it.amount }.sum()) )
-            barDataSetList.add(BarDataSet(barEntryList,Month.of(pair.first).name))
+            val barDataSet = BarDataSet(barEntryList,Month.of(pair.first).name)
+            barDataSetList.add(barDataSet)
+            context?.let{
+                barDataSet.color = ContextCompat.getColor(it,R.color.color_lightRed)
+            }
         }
         val barData = BarData(barDataSetList)
         anyChartView.data = barData
+
+        anyChartView.invalidate()
     }
 
     private fun processTexts(budget: Float, expenseSum: Float){
         dailyBudget?.text = "$${budget}"
         dailyTotal?.text = "$${expenseSum}"
-        val df : DecimalFormat = DecimalFormat("#.##")
+        val df = DecimalFormat("#.##")
         df.roundingMode = RoundingMode.CEILING
         val remainingMon = (budget - expenseSum)
         remainingMoney?.text = "$${df.format(remainingMon)}"
