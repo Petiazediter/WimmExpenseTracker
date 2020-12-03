1ec21baf882ee0f6ab4fc338947299ed33501b86
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-12-03 17:48:53 +0100

    View model to recycler view added.

--- app/src/main/java/com/codecool/wimmexpensetracker/mvvm/view_models/RecyclerAdapterViewModel.kt
+++ app/src/main/java/com/codecool/wimmexpensetracker/mvvm/view_models/RecyclerAdapterViewModel.kt
@@ -1,6 +1,22 @@
 package com.codecool.wimmexpensetracker.mvvm.view_models
 
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.codecool.wimmexpensetracker.mvvm.repositories.RecyclerAdapterRepository
+import com.codecool.wimmexpensetracker.room_db.Category
+import com.codecool.wimmexpensetracker.room_db.Expense
 
 class RecyclerAdapterViewModel : ViewModel() {
+
+    private var mRecyclerAdapterRepository : RecyclerAdapterRepository? = null
+
+    fun init(lifecycleOwner: LifecycleOwner){
+        if ( mRecyclerAdapterRepository == null){
+            mRecyclerAdapterRepository = RecyclerAdapterRepository.getInstance(lifecycleOwner)
+        }
+    }
+
+    fun getExpensesByCategory( category : Category) : MutableLiveData<List<Expense>>? = mRecyclerAdapterRepository?.getExpensesByCategoryId(category.uId)
+
 }
