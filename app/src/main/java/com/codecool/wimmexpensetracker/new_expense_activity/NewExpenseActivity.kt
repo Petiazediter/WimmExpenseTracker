7af6a3faa882ee71bd4eaa0218e1e9740a4c4699
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-12-03 20:04:41 +0100

    New expense layout implemented.

---
+++ app/src/main/java/com/codecool/wimmexpensetracker/new_expense_activity/NewExpenseActivity.kt
@@ -0,0 +1,14 @@
+package com.codecool.wimmexpensetracker.new_expense_activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.codecool.wimmexpensetracker.R
+
+class NewExpenseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_expense)
+    }
+
+}
