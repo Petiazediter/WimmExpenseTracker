74ab19d39425ad983a6b96d21a6c6bd9c5bed587
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-12-03 18:07:52 +0100

    expenses fragment implemented.

---
+++ app/src/main/java/com/codecool/wimmexpensetracker/expenses_fragment/ExpensesFragment.kt
@@ -0,0 +1,35 @@
+package com.codecool.wimmexpensetracker.expenses_fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.codecool.wimmexpensetracker.R
+import com.codecool.wimmexpensetracker.product_activity.ActivityButtonListener
+import com.codecool.wimmexpensetracker.product_activity.MainActivityContractor
+
+
+class ExpensesFragment : Fragment(), ActivityButtonListener {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_expenses, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if ( activity is MainActivityContractor){
+            (activity as MainActivityContractor).apply {
+                    setMenuTitle(resources.getString(R.string.expenses_fragment_title))
+                    setSubMenuTitle(resources.getString(R.string.expenses_fragment_subtitle))
+            }
+        }
+    }
+
+    override fun onButtonPressed() {
+
+    }
+
+}
