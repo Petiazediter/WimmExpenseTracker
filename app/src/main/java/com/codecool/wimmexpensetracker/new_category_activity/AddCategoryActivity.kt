892533a0e1a56cc25112f33a44082cd13cd06b03
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-12-03 11:18:48 +0100

    New category activity created.

---
+++ app/src/main/java/com/codecool/wimmexpensetracker/new_category_activity/AddCategoryActivity.kt
@@ -0,0 +1,12 @@
+package com.codecool.wimmexpensetracker.new_category_activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codecool.wimmexpensetracker.R
+
+class AddCategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_category)
+    }
+}
