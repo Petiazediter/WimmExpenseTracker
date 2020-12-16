72585451bf961e02758eaab1689cd1af7c5f66f9
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-12-16 12:28:18 +0100

    Koin bugs fixed with changing the koin version.

--- app/src/main/java/com/codecool/wimmexpensetracker/mvvm/KoinApplication.kt
+++ app/src/main/java/com/codecool/wimmexpensetracker/mvvm/KoinApplication.kt
@@ -7,6 +7,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 import org.koin.dsl.module
 
 val appModule = module {
@@ -35,7 +36,7 @@ class KoinApplication : Application(){
         super.onCreate()
         // Start Koin
         startKoin{
-            androidLogger()
+            androidLogger(Level.NONE)
             androidContext(this@KoinApplication)
             modules(appModule)
         }
