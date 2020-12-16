df79a5e182bfe418c184bfe95bbe4afe9c61f19d
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-12-16 10:56:25 +0100

    Layout prepared.

--- app/src/main/java/com/codecool/wimmexpensetracker/settings_fragment/SettingsFragment.kt
+++ app/src/main/java/com/codecool/wimmexpensetracker/settings_fragment/SettingsFragment.kt
@@ -6,9 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.codecool.wimmexpensetracker.R
+import com.codecool.wimmexpensetracker.product_activity.ActivityButtonListener
+import com.codecool.wimmexpensetracker.product_activity.MainActivityContractor
 
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : Fragment(),ActivityButtonListener {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -16,4 +18,20 @@ class SettingsFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
+
+    override fun onResume() {
+        super.onResume()
+        context?.let{ context ->
+            (activity as MainActivityContractor).apply {
+                setMenuTitle( context.getString(R.string.settings_fragment_title))
+                setSubMenuTitle(context.getString(R.string.home_fragment_title))
+            }
+        }
+    }
+
+    override fun onButtonPressed() {
+        // go to home fragment
+    }
+
+
 }
