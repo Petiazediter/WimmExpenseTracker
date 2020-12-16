a12229effef6665a8e12940c0cc88e283f06b27f
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-12-16 08:41:56 +0100

    Added view model to expense fragment.

--- app/src/main/java/com/codecool/wimmexpensetracker/mvvm/view_models/ExpenseFragmentViewModel.kt
+++ app/src/main/java/com/codecool/wimmexpensetracker/mvvm/view_models/ExpenseFragmentViewModel.kt
@@ -1,9 +1,21 @@
 package com.codecool.wimmexpensetracker.mvvm.view_models
 
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.codecool.wimmexpensetracker.mvvm.repositories.ExpenseFragmentRepository
+import com.codecool.wimmexpensetracker.room_db.Expense
 
 class ExpenseFragmentViewModel : ViewModel(){
 
+    private var mExpenseFragmentRepository : ExpenseFragmentRepository? = null
+    public var allExpenses : MutableLiveData<List<Expense>>? = null
 
+    fun init(lifecycleOwner: LifecycleOwner){
+        if ( mExpenseFragmentRepository == null) {
+            mExpenseFragmentRepository = ExpenseFragmentRepository.getInstance(lifecycleOwner)
+            allExpenses = mExpenseFragmentRepository?.getAllExpenses()
+        }
+    }
 
 }
